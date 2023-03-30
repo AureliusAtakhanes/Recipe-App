b70b82b50c91f8c4df63e142d30cca8936b289e1
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-03-30 17:23:54 +0600

    feat: completed navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className={styles.container}>
+
+            <div className={styles.wrapper}>
+                <Link className={styles.left}>
+                    Atakan's Recipes
+                </Link>
+                <ul className={styles.center}>
+                    <li className={styles.listItem}>Home</li>
+                    <li className={styles.listItem}>About</li>
+                    <li className={styles.listItem}>Contacts</li>
+                    <li className={styles.listItem}>Services</li>
+                </ul>
+                <div className={styles.right}>
+                    <input type="text" placeholder='Search...' />
+                    <AiOutlineSearch />
+                </div>
+            </div>
 
         </div>
     )
