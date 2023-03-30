3480e1ca1b8b8285abc0db3fa5ec896df1236810
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-03-30 13:56:25 +0600

    start project: deleted unnecessary files and created components

---
+++ src/components/favoriteFoods/FavoritesFoods.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './FavoritesFoods.module.css';
+
+const FavoritesFoods = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FavoritesFoods
---
+++ src/components/recipeDetails/RecipeDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './RecipeDetails.module.css';
+
+const RecipeDetails = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RecipeDetails
